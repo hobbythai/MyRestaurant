e93f93485979f0e6c42e351ca8efbf1e598c8bb2
ks <kally.thailand@gmail.com>
2015-09-03 15:03:12 +0700

    create input stream

--- app/src/main/java/com/hobbythai/myrestaurant/FoodTABLE.java
+++ app/src/main/java/com/hobbythai/myrestaurant/FoodTABLE.java
@@ -1,5 +1,7 @@
 package com.hobbythai.myrestaurant;
 
+import android.content.ContentValues;
+import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
 /**
@@ -11,10 +13,28 @@ public class FoodTABLE {
     private MyOpenHelper objMyOpenHelper;
     private SQLiteDatabase writeSqLiteDatabase, readSqLiteDatabase;
 
+    public static final String FOOD_TABLE = "foodTABLE";
+    public static final String COLUMN_ID_FOOD = "_id";
+    public static final String COLUMN_FOOD = "Food";
+    public static final String COLUMN_SOURCE = "Source";
+    public static final String COLUMN_PRICE = "Price";
 
-    public FoodTABLE() {
+
+    public FoodTABLE(Context context) {
+        objMyOpenHelper = new MyOpenHelper(context);
+        writeSqLiteDatabase = objMyOpenHelper.getWritableDatabase();
+        readSqLiteDatabase = objMyOpenHelper.getReadableDatabase();
 
     }//contructor
+
+    public long addFood(String strFood, String strSource, String strPrice) {
+
+        ContentValues objContentValues = new ContentValues();
+        objContentValues.put(COLUMN_FOOD, strFood);
+        objContentValues.put(COLUMN_SOURCE, strSource);
+        objContentValues.put(COLUMN_PRICE, strPrice);
+        return writeSqLiteDatabase.insert(FOOD_TABLE,null,objContentValues);
+    }
 
 }//main
 
--- app/src/main/java/com/hobbythai/myrestaurant/MainActivity.java
+++ app/src/main/java/com/hobbythai/myrestaurant/MainActivity.java
@@ -1,14 +1,26 @@
 package com.hobbythai.myrestaurant;
 
+import android.database.sqlite.SQLiteDatabase;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
     //explicit
     private UserTABLE objUserTABLE;
+    private FoodTABLE objFoodTABLE;
 
 
 
@@ -21,16 +33,73 @@ public class MainActivity extends AppCompatActivity {
         createAndConnectedDatabase();
 
         //tester add value
-        testerAddValue();
+        //testerAddValue();
+
+        //delete all data
+        deleteAllData();
+
+        //synchronize JSON to SQLite
+        synJASONtoSQLite();
+
 
     }//on create
 
+    private void synJASONtoSQLite() {
+
+        //change policy
+        StrictMode.ThreadPolicy objThreadPolicy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(objThreadPolicy);
+
+        int intTime = 0;
+        while (intTime<=1) {
+
+            InputStream objInputStream = null;
+            String strJASON = null;
+            String strUserURL = "http://www.swiftcodingthai.com/3sep/php_get_data_master.php";
+            String strFoodURL = "http://www.swiftcodingthai.com/3sep/php_get_data_food.php";
+            HttpPost objHttpPost = null;
+
+            //1.create input stream
+            try {
+
+                HttpClient objHttpClient = new DefaultHttpClient();
+                if (intTime != 1) {
+                    objHttpPost = new HttpPost(strUserURL);
+                } else {
+                    objHttpPost = new HttpPost(strFoodURL);
+                }
+
+                HttpResponse objHttpResponse = objHttpClient.execute(objHttpPost);
+                HttpEntity objHttpEntity = objHttpResponse.getEntity();
+                objInputStream = objHttpEntity.getContent();
+
+            } catch (Exception e) {
+                Log.d("Rest", "Input ==> " + e.toString());
+            }
+
+            //2.create JASON
+
+            //3.update to SQLite
+
+
+            intTime += 1;
+        }//while
+    }
+
+    private void deleteAllData() {
+        SQLiteDatabase objSqLiteDatabase = openOrCreateDatabase("Restaurant.db", MODE_PRIVATE, null);
+        objSqLiteDatabase.delete("userTABLE", null, null);
+        objSqLiteDatabase.delete("foodTABLE", null, null);
+    }
+
     private void testerAddValue() {
         objUserTABLE.addNewUser("testUser", "12345", "kullachat");
+        objFoodTABLE.addFood("กระเพรา", "testSouce", "1234");
     }
 
     private void createAndConnectedDatabase() {
         objUserTABLE = new UserTABLE(this);
+        objFoodTABLE = new FoodTABLE(this);
     }
 
     @Override
